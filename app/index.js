eaf7feb4db75bf9fb7dcb69660a8e82411b23d8e
Brian Di Palma <offler@gmail.com>
2014-04-12 17:47:15 +0100

    Clean up generator class, ask for npm package name.

--- app/index.js
+++ app/index.js
@@ -1,9 +1,7 @@
-'use strict';
-var util = require('util');
-var path = require('path');
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
+"use strict";
 
+var chalk = require('chalk');
+var yeoman = require('yeoman-generator');
 
 var NodeEs6Generator = yeoman.generators.Base.extend({
   init: function () {
@@ -11,7 +9,7 @@ var NodeEs6Generator = yeoman.generators.Base.extend({
 
     this.on('end', function () {
       if (!this.options['skip-install']) {
-        this.installDependencies();
+//        this.installDependencies();
       }
     });
   },
@@ -19,37 +17,26 @@ var NodeEs6Generator = yeoman.generators.Base.extend({
   askFor: function () {
     var done = this.async();
 
-    // have Yeoman greet the user
-    this.log(this.yeoman);
-
-    // replace it with a short and sweet description of your generator
-    this.log(chalk.magenta('You\'re using the fantastic NodeEs6 generator.'));
+    this.log(chalk.magenta('You\'re using the Node ES6 project generator.'));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'input',
+      name: 'packageName',
+      default: "es6-node-package",
+      message: 'What do you want to call your new ES6 node package?'
     }];
 
     this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+      this.packageName = props.packageName;
 
       done();
     }.bind(this));
   },
 
   app: function () {
-    this.mkdir('app');
-    this.mkdir('app/templates');
+    this.mkdir(this.packageName);
 
-    this.copy('_package.json', 'package.json');
-    this.copy('_bower.json', 'bower.json');
-  },
-
-  projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshintrc', '.jshintrc');
+    this.copy('_package.json', this.packageName + '/package.json');
   }
 });
 
